3e42eaaacd37dd4326425a9c0585f9562748d84d
matejci <ciloid@gmail.com>
2016-02-29 02:07:59 +0100

    test

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -8,6 +8,7 @@ class FilesController < ApplicationController
 		puts "import_data"
 		p params
 
+
 		#Sample.import_data(params[:attachment])
 		TestWorker.perform_in(10.seconds, 'http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv')
 		redirect_to samples_path()
